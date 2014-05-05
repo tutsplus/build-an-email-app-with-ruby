48ac2025e97ad7253dd02ed1eb2ca2a774dd87ae
José Mota <jose@josemota.net>
2014-05-05 15:31:08 +0100

    Make common code easier to extract.

--- lib/qrier/services/fetch_emails.rb
+++ lib/qrier/services/fetch_emails.rb
@@ -5,25 +5,29 @@ module Qrier
     def execute
       user = ENV['QRIER_USER']
       password = ENV['QRIER_PWD']
-      raise ArgumentError, "Please provide your email credentials." unless user && password
+      raise ArgumentError, 'Please provide your email credentials.' unless user && password
 
       begin
         imap = Net::IMAP.new 'mail.josemota.net'
         imap.login user, password
-        imap.examine 'INBOX'
+
+        recipient = self
+        folder = 'INBOX'
+
+        imap.examine folder
         imap.search(['ALL']).each do |id|
           envelope = imap.fetch(id, 'ENVELOPE')[0].attr['ENVELOPE']
           uid = imap.fetch id, 'UID'
           flags = imap.fetch id, 'FLAGS'
 
-          add_email Email.new(
+          recipient.add_email Email.new(
             from:    address(envelope.from),
             to:      address(envelope.to),
             cc:      address(envelope.cc),
             bcc:     address(envelope.bcc),
             subject: envelope.subject,
             sent_at: Time.new(envelope.date),
-            body:    imap.fetch(id, 'BODY[TEXT]')[0].attr['BODY[TEXT]'].force_encoding("utf-8"),
+            body:    imap.fetch(id, 'BODY[TEXT]')[0].attr['BODY[TEXT]'].force_encoding('utf-8'),
             new?:    !flags.include?(:Seen)
           )
         end
